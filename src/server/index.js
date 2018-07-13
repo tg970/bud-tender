876dfe5765f7a4807de434b8dfe63a82b2869671
tg970 <tgoodman@outlook.com>
2018-07-13 01:38:10 -0600

    default client code, webpack config, api responding from client

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,8 @@ const PORT = process.env.PORT || 1122;
 
 app.use(express.static('dist'));
 
+app.get('/api/hello', (req, res) => {
+  res.send({ message: "Hello World!!" });
+});
+
 app.listen(PORT, () => console.log('Server OK'));
